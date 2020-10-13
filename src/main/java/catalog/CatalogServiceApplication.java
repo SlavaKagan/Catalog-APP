88dad990ffcae6c0b37bec422e9a62cd1c536004
Slava Kagan <slava15.92@gmail.com>
2020-10-13 16:14:24 +0300

    make project with gradle as modules

---
+++ src/main/java/catalog/CatalogServiceApplication.java
@@ -0,0 +1,14 @@
+package catalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
+@SpringBootApplication
+public class CatalogServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CatalogServiceApplication.class, args);
+    }
+}
